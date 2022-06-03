2285cbcea42e47e51ee3931ddc5719baf39b0a1d
Igor <igor.basuga@gmail.com>
2022-06-03 13:25:15 +0200

    add custom 404 template

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from cgitb import handler
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 
 urlpatterns = [
@@ -26,5 +28,7 @@ urlpatterns = [
     path("products/", include("products.urls")),
     path("bag/", include("bag.urls")),
     path("checkout/", include("checkout.urls")),
-    path("profile/", include("profiles.urls")), 
+    path("profile/", include("profiles.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = "boutique_ado.views.handler404"
